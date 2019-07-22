419f94bb72781fa4ebf23aca78303984017162ef
Vivian Imbriotis <vivian.imbriotis@gmail.com>
2019-07-23 00:01:09 +1000

    "Metadata extraction implemeneted"

--- pdfscraper.py
+++ pdfscraper.py
@@ -22,14 +22,17 @@ def scrape_pdf(path, printing = False):
 
 
 def fetch_acuity_array(lines,eye):
-    field_text = lines[14:18] + lines[19:23] #The lines containing the acuity matrix
+    #The lines containing the acuity matrix
+    field_text = lines[14:18] + lines[19:23]
+    #This variable is going to be a list of lists
+    #that will be converted into a ndarray-type
     field_ints = []
     if eye == "left":
         for row in field_text:
             row = row.split(" ")
             int_row = []
             for i in range((9-len(row))//2):
-                int_row.append(0) #left padding
+                int_row.append(0) #left padding with 0
             for value in row:
                 try:
                     int_row.append(int(value))
@@ -39,16 +42,16 @@ def fetch_acuity_array(lines,eye):
                     else:
                         raise
             while len(int_row) < 9:
-                int_row.append(0) #right padding
+                int_row.append(0) #right padding with 0
             field_ints.append(int_row)
     elif eye == "right":
         for row in field_text:
             row = row.split(" ")
             int_row = []
             for i in range((9-len(row))//2):
-                int_row.append(0) #left padding
+                int_row.append(0) #left padding with 0
             if len(row)!=9:
-                int_row.append(0) #more left padding
+                int_row.append(0) #more left padding with 0
             for value in row:
                 try:
                     int_row.append(int(value))
@@ -58,7 +61,7 @@ def fetch_acuity_array(lines,eye):
                     else:
                         raise
             while len(int_row) < 9:
-                int_row.append(0) #right padding
+                int_row.append(0) #right padding with 0
             field_ints.append(int_row)
     else:
         raise ValueError("eye argument must be either \"left\" or \"right\"")
@@ -68,11 +71,29 @@ def fetch_acuity_array(lines,eye):
 class EyeExam:
     def __init__(self,path_to_pdf):
         lines = scrape_pdf(path_to_pdf, printing = False)
+        #First, we extract the identifying infomation
         self.name = lines[0][lines[0].index(" ")+1:]
         self.id = lines[3].split(" ")[2]
+        self.age = int(lines[9].split(" ")[-1])
+        #Next, quality control parametes
         self.fixation_monitor = lines[7].split(" ")[2]
         self.fixation_target = lines[8].split(" ")[2]
-        self.age = lines[9].split(" ")[-1]
+        self.fixation_losses = lines[9].split(" ")[2]
+        self.false_pos_errors = lines[10][
+            lines[10].index(":")+2:lines[10].index("%")+1]
+        self.false_neg_errors = lines[11][
+            lines[11].index(":")+2:lines[11].index("%")+1]
+        self.test_duration = lines[12][15:20]
+        self.forvea = lines[13][7:]
+        #Next, assessment details
+        self.ght = lines[29].split(": ")[1]
+        self.vfi = lines[32].split(": ")[1]
+        self.md = lines[33].split("MD: ")[1]
+        self.md = self.md[0:self.md.index("B")+1]
+        self.psd = lines[34].split(": ")[1]
+        self.psd = self.psd[0:self.psd.index("B")+1]
+        #Good practice to do error checking when fetching which eye was examined
+        #since {left, right} is the complete set of possibilites...
         if lines[6][0:2] == "OS":
             self.eye = "left"
         elif lines[6][0:2] == "OD":
@@ -80,11 +101,14 @@ class EyeExam:
         else:
             raise LookupError("Laterality encoding (\"OS\" or \"OD\")"
                               + "not found in expected location")
+        #Finally, we fetch the actual data from the visual field matrix
+        #as a numpy array (chosen because they're portable, work with
+        #tensorflow and pytorch, etc)
         self.acuity_array = fetch_acuity_array(lines,self.eye)
     def __call__(self):
         return self.acuity_array
     def __repr__(self):
-        return "Patient %s's %s eye data" %(self.id, self.eye)
+        return "%s %s eye" %(self.id, self.eye)
     def __str__(self):
         string_out = (("Patient ID: %s\n" %self.id)
                       + ("Patient Age: %s\n" %self.age)
@@ -93,6 +117,10 @@ class EyeExam:
                       + self.acuity_array.__str__()
                       +"\n")
         return string_out
+    def print_detailed(self):
+        for key in self.__dict__:
+            print("%s: %s"%(key,self.__dict__[key]))
+        
         
 
 if __name__ == "__main__":
@@ -108,3 +136,4 @@ if __name__ == "__main__":
             print("Scrapting directory content...")
             for file in os.listdir(path):
                 print(EyeExam(os.path.join(path,file)))
+
